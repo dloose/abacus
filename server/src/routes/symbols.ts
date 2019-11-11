1961f8848189e870a563edb80c7dff9ce8f7607f
Dave Loose <dloose@gmail.com>
2019-11-11 14:48:10 -0500

    added a POST route to add new symbols and run initial import

--- server/src/routes/symbols.ts
+++ server/src/routes/symbols.ts
@@ -1,6 +1,7 @@
 import * as express from "express";
 import * as pg from "pg";
 import sql from "sql-template-tag";
+import {call} from "../services/celery";
 import {connect} from "../services/database";
 import {asyncRoute} from "../util/routes";
 
@@ -24,6 +25,34 @@ export default function init(): express.Router {
             conn.release();
 
             return res.status(200).json(data.rows);
+        }));
+
+    // add a new symbol
+    router.post("/:symbol",
+        asyncRoute(async function addSymbol(req, res) {
+            const symbol = req.params.symbol;
+
+            const conn: pg.PoolClient = await connect();
+
+            // use an UPSERT to add the requested symbol to the symbols table, if necessary
+            const insertResult =
+                await conn.query(sql`
+                    INSERT INTO symbols (symbol, added_date) 
+                    VALUES (${symbol}, NOW()) 
+                    ON CONFLICT DO NOTHING
+                    RETURNING *
+                `);
+
+            conn.release();
+
+            // if the INSERT was successful, run the initial import task and wait for it to complete
+            if (insertResult.rowCount !== 0) {
+                call("tasks.initial_import", [symbol]).get().catch(err =>
+                    console.error(`Initial import failed for ${symbol}: ${err}`));
+                return res.status(201).json(insertResult.rows[0]);
+            }
+
+            return res.status(400).send(`Symbol ${symbol} already exists.`);
         }));
 
     // return data for a single symbol
